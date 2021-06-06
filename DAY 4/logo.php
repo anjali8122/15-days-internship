d2123f683f8ab2b7c35b65bd7ab156a5806c8cac
anjali8122 <68642021+anjali8122@users.noreply.github.com>
2021-06-06 22:38:51 +0530

    Update logo.php

--- DAY 4/logo.php
+++ DAY 4/logo.php
@@ -1,8 +1,8 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<div id="header-wrapper">
+	<div id="header">
+		<div id="logo">
+			<h1><a href="#">Anjali</a></h1>
+			
+		</div>
+	</div>
+</div>
