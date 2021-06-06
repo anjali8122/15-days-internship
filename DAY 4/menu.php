12122ab25230e0072fd89a17c727e77031dd7c25
anjali8122 <68642021+anjali8122@users.noreply.github.com>
2021-06-06 22:43:42 +0530

    Update menu.php

--- DAY 4/menu.php
+++ DAY 4/menu.php
@@ -1,8 +1,10 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<div id="menu-wrapper">
+	<div id="menu">
+		<ul>
+                    <li class="current_page_item"><a href="home.php">Homepage</a></li>
+                        <li><a href="about.php">About</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+		</ul>
+	</div>
+	
+</div>
