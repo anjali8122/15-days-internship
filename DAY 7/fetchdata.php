41c26a799f2a8e747de9db3ed10e4e711311ef9d
anjali8122 <68642021+anjali8122@users.noreply.github.com>
2021-06-06 20:23:30 +0530

    Update fetchdata.php

--- DAY 7/fetchdata.php
+++ DAY 7/fetchdata.php
@@ -1,6 +1,6 @@
 <?php
 
-$connection = mysqli_connect("localhost","pma","db_internship");
+$connection = mysqli_connect("localhost","root","","db_internship");
  
 //query
 $q = mysqli_query($connection,"select * from tbl_user") or die(mysqli_error($connection));
@@ -13,5 +13,7 @@ print_r($row);
 echo $row[0].$row[1].$row[2];
 
 //numerical nd associative
-$row = mysqli_fetcharray($q);
-    
+$row = mysqli_fetch_array($q);
+echo "<pre>";
+print_r($row);
+echo $row[0].$row[1].$row[2];
